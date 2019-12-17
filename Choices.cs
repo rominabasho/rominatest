70195465964dfe3c229af4c8b518611b53eccc3a
rominabasho <rominabasho@hotmail.com>
2019-12-17 17:00:29 +0100

    Added an enum for choices, default choice for the first player, and a random choice for the second player.

---
+++ Choices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RominaTest
+{
+    public enum Choices
+    {
+        Paper = 1,
+        Rock = 2,
+        Scissor = 3
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,18 @@ namespace RominaTest
     {
         static void Main(string[] args)
         {
+            var rounds = 100;
+
+            var player1Choice = Choices.Rock;
+            var rng = new Random();
+
+            for (int i = 0; i < rounds; i++)
+            {
+                var player2Choice = (Choices)rng.Next(1, 4);
+
+
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
